858bf1768dd55c777d7873ddab96872b98ddff68
Matt Rich <matthew.rich@perficient.com>
2019-06-20 10:18:06 -0600

    add findNums

---
+++ JS/findNums.js
@@ -0,0 +1,12 @@
+/**
+ * findNums should take a String, find the numbers in each word, then add them together
+ * EX:
+ *   "He3ll5o 4m5y na3me is Mor345tim2er"
+ *   35 + 45 + 3 + 3452
+ *   return 3535
+ */
+
+function fundNums (str) {
+    ...
+}
+
